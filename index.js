c076829748977db03c85141c48d07674da164803
Jarek Wojciechowski <jarekwojo@gmail.com>
2019-03-19 10:59:46 -0400

    max the size of canvas to the window size, and remove the margin from the body

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 var canvas = document.querySelector('canvas');
 var context = canvas.getContext('2d');
 
-//const size = window.innerWidth / 3;
-const size = 500;
+//const size = window.innerWidth / 2;
 const dpr = window.devicePixelRatio;
-const STEP = 20;
+const STEP = 40;
 
-canvas.width = size * dpr;
-canvas.height = size * dpr;
-//context.scale(dpr, dpr);
+canvas.width = window.innerWidth * dpr;
+canvas.height = window.innerHeight * dpr;
+context.scale(dpr, dpr);
 
 context.lineCap = 'square';
 context.lineWidth = 2;
-const MAX_DISTANCE = Math.sqrt((size * size) + (size * size));
+const MAX_DISTANCE = Math.sqrt((window.innerWidth * window.innerWidth) + (window.innerHeight * window.innerHeight));
 
 // Angle is measured from the x axis clockwise
 
